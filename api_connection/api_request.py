83e35f0c1c112bc0061c12350384b35982e3f132
FFFFFRYGIO <42962305+FFFFFRYGIO@users.noreply.github.com>
2022-12-18 02:38:13 +0100

    create api connection package

---
+++ api_connection/api_request.py
@@ -0,0 +1,33 @@
+from requests import get
+
+
+def generate_query(book_params):
+    # book_params is a dict with parameters to import book
+
+    query = ''
+
+    for param in book_params:
+        if not len(query):
+            query += param + ':' + book_params[param]
+        else:
+            query += '+' + param + ':' + book_params[param]
+
+    return query
+
+
+def get_api_request(book_params):
+    if not len(book_params):
+        flash('No parameters to add books')
+        return -1
+
+    url = r'https://www.googleapis.com/books/v1/volumes'
+
+    query = generate_query(book_params)
+    params = {"q": query}
+    response = get(url, params=params)
+    response_dict = response.json()
+
+    if response_dict['totalItems']:
+        return response_dict
+    else:
+        return -1
