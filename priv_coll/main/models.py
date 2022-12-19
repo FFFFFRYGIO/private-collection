9a16bd6a3e01fb6b2732437897f685839a35441c
FFFFFRYGIO <42962305+FFFFFRYGIO@users.noreply.github.com>
2022-12-19 01:13:32 +0100

    create Book model, give admin access to search Books

--- priv_coll/main/models.py
+++ priv_coll/main/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Book(models.Model):
+    ISBN = models.CharField(max_length=50, null=False, unique=True)
+    title = models.CharField(max_length=270, null=False)
+    authors = models.CharField(max_length=110, null=False)
+    publishedDate = models.CharField(max_length=10, null=True, default=None, blank=True)
+    pageCount = models.IntegerField(null=True, default=None, blank=True)
+    thumbnail = models.CharField(max_length=200, null=True, default=None, blank=True)
+    language = models.CharField(max_length=10, null=True, default=None, blank=True)
+    slug = models.SlugField(max_length=4, unique=True)
+    class Meta:
+        indexes = [
+            models.Index(fields=['ISBN'])
+        ]
+
+    def __str__(self):
+        return " ".join([str(self.ISBN), str(self.title)])
