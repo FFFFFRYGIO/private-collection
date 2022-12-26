3fa4007a0136b8e3e31829e44293d7eee5fb1b4c
FFFFFRYGIO <42962305+FFFFFRYGIO@users.noreply.github.com>
2022-12-26 04:09:54 +0100

    create form to add books

--- priv_coll/main/forms.py
+++ priv_coll/main/forms.py
@@ -1,13 +1,11 @@
 from django import forms
 
-key_words = ['intitle', 'inauthor', 'inpublisher', 'subject', 'isbn', 'lccn', 'oclc']
-
 
 class AddBooks(forms.Form):
-    intitle = forms.CharField(label="intitle", max_length=200, required=False)
-    inauthor = forms.CharField(label="inauthor", max_length=200, required=False)
-    inpublisher = forms.CharField(label="inpublisher", max_length=200, required=False)
-    subject = forms.CharField(label="subject", max_length=200, required=False)
-    isbn = forms.CharField(label="isbn", max_length=200, required=False)
-    lccn = forms.CharField(label="lccn", max_length=200, required=False)
-    oclc = forms.CharField(label="oclc", max_length=200, required=False)
+    intitle = forms.CharField(label='In the title', max_length=200, required=False)
+    inauthor = forms.CharField(label='In the authors', max_length=200, required=False)
+    inpublisher = forms.CharField(label='In the publisher', max_length=200, required=False)
+    subject = forms.CharField(label='In the category list', max_length=200, required=False)
+    isbn = forms.CharField(label='Exact ISBN number', max_length=200, required=False)
+    lccn = forms.CharField(label='In the Library of Congress Control Number', max_length=200, required=False)
+    oclc = forms.CharField(label='In the Online Computer Library Center number', max_length=200, required=False)
