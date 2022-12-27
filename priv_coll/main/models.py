e62bc6506d59742050986f56050d9d51c450ab59
FFFFFRYGIO <42962305+FFFFFRYGIO@users.noreply.github.com>
2022-12-27 04:06:55 +0100

    assign user to book object

--- priv_coll/main/models.py
+++ priv_coll/main/models.py
@@ -1,9 +1,10 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 
 
 class Book(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='book', null=True)
     ISBN = models.CharField(max_length=50, null=False, unique=True)
     title = models.CharField(max_length=270, null=False)
     authors = models.CharField(max_length=110, null=False)
