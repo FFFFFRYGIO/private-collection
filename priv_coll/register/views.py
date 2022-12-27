4a52bfdd49266b749c9bb9e2406e038e314a2a25
FFFFFRYGIO <42962305+FFFFFRYGIO@users.noreply.github.com>
2022-12-27 04:09:51 +0100

    manage registration

--- priv_coll/register/views.py
+++ priv_coll/register/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
-
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
 
 
 def register(response):
-    return render()
+    if response.method == "POST":
+        form = UserCreationForm(response.POST)
+        if form.is_valid():
+            form.save()
+    else:
+        form = UserCreationForm()
+
+    return render(response, "register/register.html", {'form': form})
