be88a8454a2487f5a5706389cc12e51095d9f15d
FFFFFRYGIO <42962305+FFFFFRYGIO@users.noreply.github.com>
2022-12-27 04:13:21 +0100

    manage main views (get/post methods, flashing messages executing requested actions)

--- priv_coll/main/views.py
+++ priv_coll/main/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import Book
-from .forms import AddBooks
+from .forms import AddBooks, AddNewBook
 from . import manage_books
 # Create your views here.
 
@@ -12,7 +12,36 @@ def home(response):
     return render(response, 'main/home.html', {})
 
 
-def books_list(response):
+def new_book(response):
+    messages = []
+    if response.method == "POST":
+        form = AddNewBook(response.POST)
+        if form.is_valid():
+            if Book.objects.filter(ISBN=form.cleaned_data["ISBN"]):
+                messages.append("Book with the ISBN " + form.cleaned_data["ISBN"] + " already exists!")
+                return render(response, 'main/new_book.html', {'messages': messages, 'form': AddNewBook})
+            else:
+                b = Book()
+                b.ISBN = form.cleaned_data["ISBN"]
+                b.authors = form.cleaned_data["authors"]
+                b.publishedDate = form.cleaned_data["publishedDate"]
+                b.pageCount = form.cleaned_data["pageCount"]
+                b.thumbnail = form.cleaned_data["thumbnail"]
+                b.language = form.cleaned_data["language"]
+                # response.user.book.add(b)
+                b.save()
+                messages.append("Book succesfully added!")
+                # return redirect('books_list', messages_parsed=messages)
+                return render(response, 'main/new_book.html', {'form': AddNewBook, 'messages': messages})
+        else:
+            messages.append('Input is not valid!')
+    return render(response, 'main/new_book.html', {'form': AddNewBook, 'messages': messages})
+
+
+def books_list(response, messages_parsed=None):
+    messages = []
+    if messages_parsed:
+        messages += messages_parsed
     if response.method == 'POST' and response.POST.get("operation"):
         operation = response.POST.get("operation")[0]
         book_isbn = response.POST.get("operation")[1:]
@@ -21,14 +50,14 @@ def books_list(response):
             return render(response, 'main/edit_book.html', {'book': book})
         elif operation == 'D':  # Delete
             Book.objects.filter(ISBN=book_isbn).delete()
-            return render(response, 'main/books_list.html', {
-                'books_list': Book.objects.all(), 'attributes_keys': attributes_keys})
-    else:
-        return render(response, 'main/books_list.html', {
-            'books_list': Book.objects.all(), 'attributes_keys': attributes_keys})
+            messages.append("Book successfully deleted!")
+    books = Book.objects.all()
+    return render(response, 'main/books_list.html', {
+        'books_list': books, 'attributes_keys': attributes_keys, 'messages': messages})
 
 
 def add_books(response):
+    messages = []
     if response.method == 'POST':
         form = AddBooks(response.POST)
         if form.is_valid():
@@ -37,22 +66,14 @@ def add_books(response):
                 if form.cleaned_data[i]:
                     book_params[i] = form.cleaned_data[i]
             result = manage_books.add_books(book_params)
-            if result.success:
-                pass
-                # messages.add_message('Adding succesful with ' + str(result.success) + ' successes')
-            else:
-                pass
-                # messages.add_message('No books added')
-            if result.errors:
-                pass
-                # messages.add_message('Errors with lack of ISBN number: ' + str(result.errors))
-            if result.duplicates:
-                pass
-                # messages.add_message('Errors with duplicated books: ' + str(result.duplicates))
-            books_list = Book.objects.all()
-            return redirect('books_list')
-    else:
-        return render(response, 'main/add_books.html', {'form': AddBooks()})
+            messages.append('Adding succesful with ' + str(result.success) + ' successes') if result.success else 0
+            messages.append('No books added') if result.success == 0 else 0
+            messages.append('Errors with lack of ISBN number: ' + str(result.errors)) if result.errors else 0
+            messages.append('Errors with duplicated books: ' + str(result.duplicates)) if result.duplicates else 0
+            return redirect('books_list', messages_parsed=messages)
+        else:
+            messages.append('Input is not valid!')
+    return render(response, 'main/add_books.html', {'form': AddBooks(), 'messages': messages})
 
 
 def edit_book(response):
