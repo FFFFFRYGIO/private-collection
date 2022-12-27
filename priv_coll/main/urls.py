8418410e5cf1893ba1cc52816b75fe391425b974
FFFFFRYGIO <42962305+FFFFFRYGIO@users.noreply.github.com>
2022-12-27 04:09:16 +0100

    path for new book creation and registering

--- priv_coll/main/urls.py
+++ priv_coll/main/urls.py
@@ -1,5 +1,5 @@
-from django.urls import path
-
+from django.urls import path, include
+from register import views as v
 from . import views
 
 urlpatterns = [
@@ -7,4 +7,7 @@ urlpatterns = [
     path("books_list/", views.books_list, name="books_list"),
     path("add_books/", views.add_books, name="add_books"),
     path("edit_book/", views.edit_book, name="edit_books"),
+    path("new_book/", views.new_book, name="new_book"),
+    path("register/", v.register, name="register"),
+    path("", include('django.contrib.auth.urls')),
 ]
